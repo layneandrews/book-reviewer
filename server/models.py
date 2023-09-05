5c186bc0986c4b4398fd46632082376f5d535eeb
Layne <saxalayne@gmail.com>
2023-09-05 14:28:00 -0600

    altered models.py

--- server/models.py
+++ server/models.py
@@ -6,40 +6,35 @@ from sqlalchemy.ext.hybrid import hybrid_property
 
 db = SQLAlchemy()
 
-class User(db.Model, SerializerMixin):
+class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
-    fname = db.Column(db.String)
-    lname = db.Column(db.String)
+    name = db.Column(db.String)
     username = db.Column(db.String)
-    email = db.Column(db.String)
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
-    _password_hash = db.Column(db.String)     
-
-    serialize_rules = ("-_password_hash","-admin", "-created_at", "-updated_at")
-
+         
 class Book(db.Model, SerializerMixin):
     __tablename__ = 'books'
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String)
     author = db.Column(db.String)
-    description = db.Column(db.String)
-    image = db.Column(db.String)
+    description = db.Column(db.Text)
+    image_url = db.Column(db.String)
     genres = db.relationship('Genre', secondary='book_genres', back_populates='books')
 
 class Review(db.Model, SerializerMixin):
     __tablename__ = 'reviews'
     id = db.Column(db.Integer, primary_key=True)
     rating = db.Column(db.Integer)
-    text = db.Column(db.String)
+    text = db.Column(db.Text)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
     book_id = db.Column(db.Integer, db.ForeignKey('books.id'))
 
 class Genre(db.Model, SerializerMixin):
     __tablename__ = 'genres'
     id = db.Column(db.Integer, primary_key=True)
-    genre = db.Column(db.String)
+    genre_name = db.Column(db.String)
     books = db.relationship('Book', secondary='book_genres', back_populates='genres')
 
 class Book_genre(db.Model):
