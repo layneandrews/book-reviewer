55f4c92c3334548b6e175361963c2bafc201d0da
Layne <saxalayne@gmail.com>
2023-09-05 15:22:34 -0600

    changed stuff

--- server/models.py
+++ server/models.py
@@ -6,22 +6,10 @@ from sqlalchemy.ext.hybrid import hybrid_property
 
 db = SQLAlchemy()
 
-class User(db.Model):
-    __tablename__ = 'users'
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    username = db.Column(db.String)
-    created_at = db.Column(db.DateTime, server_default=db.func.now())
-    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
-         
-class Book(db.Model, SerializerMixin):
-    __tablename__ = 'books'
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String)
-    author = db.Column(db.String)
-    description = db.Column(db.Text)
-    image_url = db.Column(db.String)
-    genres = db.relationship('Genre', secondary='book_genres', back_populates='books')
+book_genres = db.Table('book_genres',
+    db.Column('book_id', db.Integer, db.ForeignKey('books.id'), primary_key=True),
+    db.Column('genre_id', db.Integer, db.ForeignKey('genres.id'), primary_key=True)
+)
 
 class Review(db.Model, SerializerMixin):
     __tablename__ = 'reviews'
@@ -31,14 +19,31 @@ class Review(db.Model, SerializerMixin):
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
     book_id = db.Column(db.Integer, db.ForeignKey('books.id'))
 
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    username = db.Column(db.String)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+    reviews = db.relationship('Review', backref='user', lazy=True)
+    books = db.relationship('Book', backref='users', secondary=Review.__table__)
+    #auth    
+
+class Book(db.Model, SerializerMixin):
+    __tablename__ = 'books'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String)
+    author = db.Column(db.String)
+    description = db.Column(db.Text)
+    image_url = db.Column(db.String)
+    genres = db.relationship('Genre', secondary=book_genres, back_populates='books')
+    reviews = db.relationship('Review', backref='Book', lazy=True)
+
 class Genre(db.Model, SerializerMixin):
     __tablename__ = 'genres'
     id = db.Column(db.Integer, primary_key=True)
     genre_name = db.Column(db.String)
-    books = db.relationship('Book', secondary='book_genres', back_populates='genres')
+    books = db.relationship('Book', secondary=book_genres, back_populates='genres')
 
-class Book_genre(db.Model):
-    __tablename__ = 'book_genres'
-    db.Column('book_id', db.Integer, db.ForeignKey('books.id'), primary_key=True)
-    db.Column('genre_id', db.Integer, db.ForeignKey('genres.id'), primary_key=True)
 
--- server/seed.py
+++ server/seed.py
@@ -1,3 +1,37 @@
 from app import app
 from models import db, User, Book, Review, Genre
 
+users = []
+
+u1 = User(name='Layne', username='landrews1127')
+users.append(u1)
+u2 = User(name='Garrett', username ='gbob99')
+users.append(u2)
+
+db.session.add_all(users)
+db.session.commit()
+
+#------------------------#
+
+genres = []
+
+g1 = Genre(genre_name='horror')
+genres.append(g1)
+g2 = Genre(genre_name='comedy')
+genres.append(g2)
+
+db.session.add_all(genres)
+db.session.commit()
+
+#------------------------#
+
+books = []
+
+b1 = Book(title='holes', author='Louis Sachar', description='A book about a boy, wrongfully convicted of theft who has to go to a desert boot camp and dig holes to pay for his crimes.', image_url='https://i0.wp.com/www.nationalbook.org/wp-content/uploads/2015/12/holes-louis-sachar-book-cover.jpg?fit=500%2C761&ssl=1')
+books.append(b1)
+db.session.add_all(books)
+db.session.commit()
+
+
+
+
